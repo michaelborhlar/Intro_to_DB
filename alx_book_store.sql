729994675a86bd6dda2397a8927066d7bef68d85
michaelborhlar <michealadefehinti09@gmail.com>
2025-07-06 18:07:18 +0100

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE  IF NOT EXISTS 'alx_book_store' /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE  IF NOT EXISTS `alx_book_store` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `alx_book_store`;
 -- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
 --
