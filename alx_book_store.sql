020a87447bedae658542d42a8f65435625b948d2
michaelborhlar <michealadefehinti09@gmail.com>
2025-07-06 18:36:19 +0100

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -76,7 +76,7 @@ DROP TABLE IF EXISTS Customers;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE Customers (
-  customer_id int NOT NULL,
+  customer_id int auto_increment NOT NULL,
   customer_name varchar(215) DEFAULT NULL,
   email varchar(215) DEFAULT NULL,
   address text,
